f4b2010f44d5dc89ca02eaa1250b7645bbfe2e45
DuyNQ18 <duynq2197@gmail.com>
2026-05-23 08:36:51 +0700

    feat: big update

---
+++ extends.js
@@ -0,0 +1,18 @@
+// 1. Tự động bơm (Inject) mã Google AdSense vào thẻ head
+const adsenseScript = document.createElement('script');
+adsenseScript.async = true;
+adsenseScript.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9578687010789825';
+adsenseScript.setAttribute('crossorigin', 'anonymous');
+document.head.appendChild(adsenseScript);
+
+// 2. Tự động bơm (Inject) thư viện Google Analytics (gtag.js) vào thẻ head
+const gaScript = document.createElement('script');
+gaScript.async = true;
+gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-RB2D382ZY8';
+document.head.appendChild(gaScript);
+
+// 3. Khởi tạo cấu hình Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-RB2D382ZY8');
